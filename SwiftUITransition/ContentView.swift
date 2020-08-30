c43ba585472335c41856000dce6a7f7cf2b7acb0
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-08-31 01:49:39 +0300

    Combining the scale, offset, and opacity transition
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

--- SwiftUITransition/ContentView.swift
+++ SwiftUITransition/ContentView.swift
@@ -9,8 +9,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var show = false
+    
     var body: some View {
-        Text("Hello, World!")
+        
+        VStack {
+            
+            RoundedRectangle(cornerRadius: 10)
+                .frame(width: 300, height: 300)
+                .foregroundColor(.pink)
+                .overlay(
+                    Text("Show Details")
+                        .font(.system(.largeTitle, design: .rounded))
+                        .bold()
+                        .foregroundColor(.white)
+                )
+            
+            if show {
+                RoundedRectangle(cornerRadius: 10)
+                    .frame(width: 300, height: 300)
+                    .foregroundColor(.yellow)
+                    .overlay(
+                        Text("Well, here is the details")
+                            .font(.system(.largeTitle, design: .rounded))
+                            .bold()
+                            .foregroundColor(.white)
+                    )
+//                    .transition(.scale(scale: 0, anchor: .bottom))
+//                    .transition(.offset(x: -600, y: 0))
+//                    .transition(AnyTransition.offset(x: -600, y: 0).combined(with: .scale))
+//                    .transition(AnyTransition.offset(x: -600, y: 0).combined(with: .scale).combined(with: .opacity))
+                    .transition(.offsetScaleOpacity)
+            }
+        }
+        .onTapGesture {
+            withAnimation(Animation.spring()) {
+                self.show.toggle()
+            }
+        }
+    }
+}
+
+extension AnyTransition {
+    static var offsetScaleOpacity: AnyTransition {
+        AnyTransition.offset(x: -600, y: 0).combined(with: .scale).combined(with: .opacity)
     }
 }
 
